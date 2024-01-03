73eda878a9a6db832e262703e84de2b994cdd14d
Henry Marques <henry.pdts@gmail.com>
2024-01-03 00:09:08 -0300

    url production adjust

--- public/script/script.js
+++ public/script/script.js
@@ -46,11 +46,14 @@ url_to_head(
 
       createOrder: async function (data, actions) {
         //https://developer.paypal.com/docs/api/orders/v2/#orders_create
-        const response = await fetch("http://localhost/create_order", {
-          method: "post",
-          headers: { "Content-Type": "application/json; charset=utf-8" },
-          body: JSON.stringify({ intent: intent }),
-        });
+        const response = await fetch(
+          "https://paypal-test-85mo.onrender.com/create_order",
+          {
+            method: "post",
+            headers: { "Content-Type": "application/json; charset=utf-8" },
+            body: JSON.stringify({ intent: intent }),
+          }
+        );
         const order = await response.json();
         return order.id;
       },
@@ -59,14 +62,17 @@ url_to_head(
         let order_id = data.orderID;
         console.log(intent);
         try {
-          const response = await fetch("http://localhost/complete_order", {
-            method: "post",
-            headers: { "Content-Type": "application/json; charset=utf-8" },
-            body: JSON.stringify({
-              intent: intent,
-              order_id: order_id,
-            }),
-          });
+          const response = await fetch(
+            "https://paypal-test-85mo.onrender.com/complete_order",
+            {
+              method: "post",
+              headers: { "Content-Type": "application/json; charset=utf-8" },
+              body: JSON.stringify({
+                intent: intent,
+                order_id: order_id,
+              }),
+            }
+          );
           const order_details = await response.json();
           console.log(order_details);
 
